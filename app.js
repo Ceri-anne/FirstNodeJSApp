51eb468931cf77558440c0be3f2bad947c0e89b8
Ceri-anne Jackson <ceri@ceri-anne.co.uk>
2016-09-18 15:49:06 +0100

    Added printMessage function

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 //Problem: We need a simple way to look at a user's badge count and Javascript points
 //Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
+function printMessage(username, badgeCount, points) {
+	var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in Javascript";
+	console.log(message);
+}
+
 //Connect to the API  (http://teamtreehousecom/username.json)
 //Read the data
 //Parse the data
 //Print the data
+printMessage("CerianneJackson",1000,2000000);
